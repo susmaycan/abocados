4b18ae07460fc75a488e23875a8984b05cfed68f
asdy <adoptdontbuy2019@gmail.com>
2020-09-03 16:10:06 +0200

    Add 404 error page

---
+++ pages/_.vue
@@ -0,0 +1,17 @@
+<template>
+  <Container>
+    <Subtitle>Error 404</Subtitle>
+    <p>'Cause I still haven't found what you are looking for 🎵</p>
+  </Container>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator'
+  import Subtitle from '~/components/Subtitle.vue'
+
+  @Component({
+    components: {Subtitle}
+  })
+  export default class Error extends Vue {
+  }
+</script>
