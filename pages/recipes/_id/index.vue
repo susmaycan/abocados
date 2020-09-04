f7e45428fee474d04771fb7ad74da0500fb4b5ef
asdy <adoptdontbuy2019@gmail.com>
2020-09-04 09:03:04 +0200

    Add detail recipe page

--- pages/recipes/_id/index.vue
+++ pages/recipes/_id/index.vue
@@ -1,16 +1,48 @@
 <template>
   <Container>
-      <img class="recipe-img":src="recipe.picture"/>
+    <div class="recipe-header">
       <Subtitle>{{recipe.name}}</Subtitle>
-      <p v-if="recipe.duration !== undefined">Duration: {{recipe.duration}} min</p>
+      <Ranking :rankingList="recipe.ranking"/>
+    </div>
+    <div class="recipe-detail-container">
+      <div class="container-left">
+        <img class="recipe-img" :src="recipe.picture"/>
+      </div>
+      <div class="container-right">
+        <p v-if="recipe.duration !== undefined">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+               class="feather feather-clock">
+            <circle cx="12" cy="12" r="10"></circle>
+            <polyline points="12 6 12 12 16 14"></polyline>
+          </svg>
+          {{recipe.duration}} min
+        </p>
+        <div>
+          <h3>Ingredients</h3>
+          <ul>
+            <li v-for="(ingredient, index) in recipe.ingredients" :key="index" v-if="recipe.ingredients !== undefined">
+              {{ingredient}}
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3>Directions</h3>
+          <p v-if="recipe.directions !== undefined">{{recipe.directions}}</p>
+        </div>
+      </div>
+    </div>
   </Container>
 </template>
 
 <script lang="ts">
   import {Component, Vue} from 'vue-property-decorator'
   import RecipesAPI from '~/api/recipes'
+  import Ranking from "~/components/Ranking.vue";
 
-  @Component
+  @Component({
+    components: {Ranking}
+  })
   export default class RecipeList extends Vue {
     private recipe: object[] = []
 
@@ -33,9 +65,35 @@
 </script>
 
 <style>
-  .recipe-img {
-    width: 300px;
-    height: 300px;
-    object-fit: cover;
+
+  .recipe-detail-container {
+    display: flex;
+    flex-direction: row;
+    column-gap: 2rem;
   }
+
+  .recipe-img {
+    width: 250px;
+    height: 250px;
+    object-fit: cover;
+    border-radius: .5rem;
+  }
+
+  .container-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    text-align: left;
+    row-gap: 1rem;
+  }
+
+  .recipe-header {
+    margin-bottom: 2rem;
+  }
+
+  .feather-clock{
+    margin-bottom: -.4rem;
+  }
+
 </style>
