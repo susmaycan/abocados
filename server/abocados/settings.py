eb9515549d89c77db669aebbf63a1da25a69edb9
Susana Mayo <suanamayo@gmail.com>
2022-07-26 13:23:34 +0200

    another .env

--- server/abocados/settings.py
+++ server/abocados/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-#(@sl)aq!5lrdd^3#mxz_!p=@tzf55oe24^%m1g^ltwedckk!k'
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
