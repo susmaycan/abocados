9498852140859fd11bb22d8045ecc919fb766b1d
Susana Mayo <suanamayo@gmail.com>
2022-07-27 09:14:11 +0200

    Add allowed hosts

--- server/abocados/settings.py
+++ server/abocados/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '167.99.38.227'
+]
 
 
 # Application definition
