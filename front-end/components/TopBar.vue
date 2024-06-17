a91ade419fea7816a5b8854a512a81002afc884a
asdy <suanamayo@gmail.com>
2024-06-17 20:54:32 +0200

    Add component library

---
+++ front-end/components/TopBar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const links = [
+  {
+    label: 'Profile',
+    avatar: {
+      src: 'https://avatars.githubusercontent.com/u/739984?v=4',
+    },
+    badge: 100,
+  },
+  {
+    label: 'Installation',
+    icon: 'i-heroicons-home',
+    to: '/getting-started/installation',
+  },
+  {
+    label: 'Vertical Navigation',
+    icon: 'i-heroicons-chart-bar',
+    to: '/components/command-palette',
+  },
+  {
+    label: 'Command Palette',
+    icon: 'i-heroicons-command-line',
+    to: '/components/command-palette',
+  },
+]
+</script>
+
+<template>
+  <div class="flex items-center">
+    <div>abocados</div>
+    <div class="ml-auto">
+      <theme-switcher />
+    </div>
+  </div>
+</template>
---
+++ front-end/layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <top-bar />
+    <div class="flex items-center">
+      <sidebar />
+      <slot />
+    </div>
+  </div>
+</template>
