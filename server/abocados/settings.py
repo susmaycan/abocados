58b98f033137ed015a19abf16a63b97dfe6055bd
asdy <suanamayo@gmail.com>
2024-06-17 18:05:06 +0200

    update url

--- server/abocados/settings.py
+++ server/abocados/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "abocados-api.onrender.com", "abocados.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "abocados-api.onrender.com", "abocados-kuaj.onrender.com", "abocados.onrender.com"]
 
 
 CORS_ORIGIN_ALLOW_ALL=True
